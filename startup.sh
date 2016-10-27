055981227a679e07dfacfa4085b2bd6e0d97b895
maany <imptodefeat@gmail.com>
2016-10-27 11:25:35 +0530

    fix JRE params, add startup.sh \n 1) ./startup.sh \n wait for openmrs-platform to start \n 2) ./startup.sh --fix-jaxen

---
+++ startup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#variable declarations
+TOMCAT_CLASSPATH=/usr/local/tomcat/lib
+OPENMRS_CLASSPATH=/usr/local/tomcat/webapps/openmrs/WEB-INF/lib
+OPENMRS_CONTEXT=/usr/local/tomcat/webapps/openmrs/WEB-INF/web.xml
+
+function build {
+	echo "Building Image"
+        docker build -t openmrs-platform:1.11.4 .
+}
+function run {
+	echo "Stopping containers"
+	docker-compose down
+	echo "Starting Container"
+	docker-compose up
+}
+function nameOfLastContainer {
+	local CONTAINER=$(docker ps -l | awk '{print $NF}'| sed -n 2p)
+	echo $CONTAINER
+	return 0
+}
+function copyJaxenTest {
+	local CONTAINER=$(nameOfLastContainer)
+	echo $(docker exec -it $CONTAINER cp -v $TOMCAT_CLASSPATH/jaxen-1.1.6.jar  $OPENMRS_CLASSPATH)
+	echo "realoding Context"
+	docker exec -it $CONTAINER touch $OPENMRS_CONTEXT
+}
+
+
+if [ "$1" == "--fix-jaxen" ]; then
+	echo $(copyJaxenTest)
+else
+		build
+		run
+fi
